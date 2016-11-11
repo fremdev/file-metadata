1fd530efb9da40d6b60462bbf5daa78131d976ff
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-11 13:30:58 +0200

    Add POST /upload route and basic html form

--- server/server.js
+++ server/server.js
@@ -5,8 +5,23 @@ const multer = require('multer');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
 app.use(express.static(publicPath));
+
+app.post('/upload', upload.single('data'), (req, res, next) => {
+  if(req.file) {
+    res.json({
+      filename: req.file.originalname,
+      size: req.file.size,
+      type: req.file.mimetype
+    });
+  } else {
+    res.status(500).json({error: 'File was not uploaded'});
+  }
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
